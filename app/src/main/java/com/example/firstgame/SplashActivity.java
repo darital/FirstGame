ada8960a9ca7d7131e15d524e03366faf21b896b
darital <igrok_791@mail.ru>
2020-09-30 20:09:10 +0500

    added timer in SplashActivity

--- app/src/main/java/com/example/firstgame/SplashActivity.java
+++ app/src/main/java/com/example/firstgame/SplashActivity.java
@@ -6,33 +6,38 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 
 import com.victor.loading.newton.NewtonCradleLoading;
 
-public class SplashActivity extends Activity implements View.OnClickListener {
+public class SplashActivity extends Activity {
     NewtonCradleLoading newtonCradleLoading;
-    Button next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         newtonCradleLoading = (NewtonCradleLoading)findViewById(R.id.newton_cradle_loading);
-        next = (Button) findViewById(R.id.next);
-        next.setOnClickListener(this);
 
-        newtonCradleLoading.start();
         newtonCradleLoading.setLoadingColor(Color.YELLOW);
-    }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.next:
+        new CountDownTimer(3000,1000){
+
+            @Override
+            public void onTick(long l) {
+                newtonCradleLoading.start();
+            }
+
+            @Override
+            public void onFinish() {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
-                break;
-        }
+                finish();
+            }
+        }.start();
+
+
     }
 }
