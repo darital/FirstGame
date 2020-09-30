3e39260ced0fe4f20e0aaca8cf24b93c5da9691c
DATA <muhammad4302001@gmail.com>
2020-09-30 21:39:14 +0300

    TOza

--- app/src/main/java/com/example/firstgame/NewGame.java
+++ app/src/main/java/com/example/firstgame/NewGame.java
@@ -4,12 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View;
+import android.widget.Button;
 
-public class NewGame extends Activity {
+public class NewGame extends Activity implements View.OnClickListener {
+
+    Button btnOk, btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(this);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnCancel:
+                finish();
+                break;
+        }
     }
 }
+
