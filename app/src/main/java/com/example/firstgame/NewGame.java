4c2c471c820f70f306e4b82f47476502d1857375
darital <igrok_791@mail.ru>
2020-09-30 21:43:31 +0500

    backend new game done

--- app/src/main/java/com/example/firstgame/NewGame.java
+++ app/src/main/java/com/example/firstgame/NewGame.java
@@ -3,6 +3,7 @@ package com.example.firstgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,11 @@ public class NewGame extends Activity implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnCancel:
+                finish();
+                break;
+            case R.id.btnOk:
+                Intent intent = new Intent(NewGame.this, MainActivity.class);
+                startActivity(intent);
                 finish();
                 break;
         }
