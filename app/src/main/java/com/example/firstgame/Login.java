bce90cc2a74a0394db56d4835fef85dd9bdedff8
shuhrat <shuhrat.sultanov12.07.1980@gmail.com>
2020-10-05 20:23:33 +0500

    login

---
+++ app/src/main/java/com/example/firstgame/Login.java
@@ -0,0 +1,46 @@
+package com.example.firstgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    EditText edt1, edt2;
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edt1 = (EditText) findViewById(R.id.edt1);
+        edt2 = (EditText) findViewById(R.id.edt2);
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn1:
+                String login = edt1.getText().toString();
+                String parol = edt2.getText().toString();
+
+                if (login.equals("darital") && parol.equals("data")) {
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(this, "Janob iltimos ma'lumot kiriting", Toast.LENGTH_SHORT).show();
+                    edt1.setText("");
+                    edt2.setText("");
+                }
+
+
+        }
+    }
+}
