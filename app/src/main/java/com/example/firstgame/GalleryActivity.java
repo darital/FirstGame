cd147be73e160921459b448bd163fb5f6c9e2313
AndroidUz98 <nqodirov1221@gmail.com>
2020-10-07 21:33:09 +0500

    backgrount

--- app/src/main/java/com/example/firstgame/GalleryActivity.java
+++ app/src/main/java/com/example/firstgame/GalleryActivity.java
@@ -1,6 +1,5 @@
 package com.example.firstgame;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,74 +10,88 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class GalleryActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ImageView imageView53;
-    ImageView imageView4;
-    ImageView imageView5;
-    ImageView imageView6;
-    ImageView imageView7;
-    ImageView imageView8;
-    ImageView imageView3;
-    TextView textView;
+    ImageView img_khiwa1, img_khiwa2, img_khiwa3, img_khiwa4, imageMain;
 
-     @Override
+    TextView textView;
+    String bino;
+    @Override
      protected void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_gallery);
          findId();
          Intent intent = getIntent();
-         String bino = intent.getStringExtra("bino");
+         bino = intent.getStringExtra("bino");
          switch (bino){
              case "kaltaminor":
                  textView.setText(R.string.kalta_minor);
+                 img_khiwa1.setImageResource(R.drawable.ichonqala);
+                 img_khiwa2.setImageResource(R.drawable.kalta_minor1);
+                 img_khiwa3.setImageResource(R.drawable.minor_k);
+                 img_khiwa4.setImageResource(R.drawable.mamirxonmadrasa);
                  break;
+
              case "nurullaboysaroyi":
                  textView.setText(R.string.nurullaboy_s);
+                 img_khiwa1.setImageResource(R.drawable.nurulla_1);
+                 img_khiwa2.setImageResource(R.drawable.nurulla_2);
+                 img_khiwa3.setImageResource(R.drawable.nurulla_3);
+                 img_khiwa4.setImageResource(R.drawable.nurullaboysaroyi);
                  break;
          }
      }
 
      @Override
      public void onClick(View view) {
-         switch (view.getId()){
-             case R.id.imageView53:
-                 textView.setText(R.string.ichon_qala);
+         switch (bino){
+             case "kaltaminor":
+                switch (view.getId()){
+                     case R.id.img_khiwa1:
+                         imageMain.setImageResource(R.drawable.ichonqala);
+                         break;
+                     case R.id.img_khiwa2:
+                         imageMain.setImageResource(R.drawable.kalta_minor1);
+                         break;
+                     case  R.id.img_khiwa3:
+                         imageMain.setImageResource(R.drawable.minor_k);
+                         break;
+                     case  R.id.img_khiwa4:
+                         imageMain.setImageResource(R.drawable.mamirxonmadrasa);
+                         break;
+
+                 }
                  break;
-             case R.id.imageView4:
-                 textView.setText(R.string.kalta_minor );
-                 break;
-             case  R.id.imageView5:
-                 textView.setText(R.string.koxna_ark);
-                 break;
-             case  R.id.imageView6:
-                 textView.setText(R.string.mamirxon_m);
-                 break;
-             case  R.id.imageView7:
-                 textView.setText(R.string.nurullaboy_s);
-                 break;
-             case  R.id.imageView8:
-                 textView.setText(R.string.pahlavon_m);
-                 break;
-             case  R.id.imageView3:
-                 textView.setText(R.string.tosh_xovli);
+
+             case "nurullaboysaroyi":
+               switch (view.getId()){
+                     case R.id.img_khiwa1:
+                         imageMain.setImageResource(R.drawable.nurulla_1);
+                         break;
+                     case R.id.img_khiwa2:
+                         imageMain.setImageResource(R.drawable.nurulla_2);
+                         break;
+                     case  R.id.img_khiwa3:
+                         imageMain.setImageResource(R.drawable.nurulla_3);
+                         break;
+                     case  R.id.img_khiwa4:
+                         imageMain.setImageResource(R.drawable.nurullaboysaroyi);
+                         break;
+
+                 }
                  break;
          }
      }
 
      private void findId(){
-         imageView53 = (ImageView) findViewById(R.id.imageView53);
-         imageView53.setOnClickListener(this);
+         img_khiwa1 = (ImageView) findViewById(R.id.img_khiwa1);
+         img_khiwa1.setOnClickListener(this);
          textView = (TextView) findViewById(R.id.textView3);
-         imageView4 = (ImageView) findViewById(R.id.imageView4);
-         imageView4.setOnClickListener(this);
-         imageView5 = (ImageView) findViewById(R.id.imageView5);
-         imageView5.setOnClickListener(this);
-         imageView6 = (ImageView) findViewById(R.id.imageView6);
-         imageView6.setOnClickListener(this);
-         imageView7 = (ImageView) findViewById(R.id.imageView7);
-         imageView7.setOnClickListener(this);
-         imageView8 = (ImageView) findViewById(R.id.imageView8);
-         imageView8.setOnClickListener(this);
-         imageView3 = (ImageView) findViewById(R.id.imageView3);
-         imageView3.setOnClickListener(this);
+         img_khiwa2 = (ImageView) findViewById(R.id.img_khiwa2);
+         img_khiwa2.setOnClickListener(this);
+         img_khiwa3 = (ImageView) findViewById(R.id.img_khiwa3);
+         img_khiwa3.setOnClickListener(this);
+         img_khiwa4 = (ImageView) findViewById(R.id.img_khiwa4);
+         img_khiwa4.setOnClickListener(this);
+         imageMain = (ImageView) findViewById(R.id.imageMain);
+         img_khiwa4.setOnClickListener(this);
      }
  }
