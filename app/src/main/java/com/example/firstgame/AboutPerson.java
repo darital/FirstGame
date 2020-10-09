75304bbccfa40a8d89d5b9ba3274ddcc0aa6f969
itexnoplus <itexnoplus@gmail.com>
2020-10-09 19:59:12 +0500

    database

---
+++ app/src/main/java/com/example/firstgame/AboutPerson.java
@@ -0,0 +1,143 @@
+package com.example.firstgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AboutPerson extends AppCompatActivity implements View.OnClickListener {
+    EditText etName, etSurname, etPhone;
+    Button btnAdd, btnRead, btnClear;
+    DBHelper dbHelper;
+    String LOG_TAG = "my_log";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_person);
+        findId();
+
+        // создаем объект для создания и управления версиями БД
+        dbHelper = new DBHelper(this);
+
+    }
+
+
+
+
+    private void findId(){
+        etName = (EditText) findViewById(R.id.etName);
+        etSurname = (EditText) findViewById(R.id.etSurname);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnAdd.setOnClickListener(this);
+        btnRead.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // создаем объект для данных
+        ContentValues cv = new ContentValues();
+
+        // получаем данные из полей ввода
+        String name = etName.getText().toString();
+        String surname = etSurname.getText().toString();
+        String phone = etPhone.getText().toString();
+
+        // подключаемся к БД
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        switch (view.getId()) {
+            case R.id.btnAdd:
+                Log.d(LOG_TAG, "--- Insert in mytable: ---");
+                // подготовим данные для вставки в виде пар: наименование столбца - значение
+
+                cv.put("name", name);
+                cv.put("surname", surname);
+                cv.put("phone", phone);
+                // вставляем запись и получаем ее ID
+                long rowID = db.insert("mytable", null, cv);
+                Log.d("my_log", "row inserted, ID = " + rowID);
+                etName.setText("");
+                etSurname.setText("");
+                etPhone.setText("");
+                break;
+            case R.id.btnRead:
+                Log.d(LOG_TAG, "--- Rows in mytable: ---");
+                // делаем запрос всех данных из таблицы mytable, получаем Cursor
+                Cursor c = db.query("mytable", null, null, null, null, null, null);
+
+                // ставим позицию курсора на первую строку выборки
+                // если в выборке нет строк, вернется false
+                if (c.moveToFirst()) {
+
+                    // определяем номера столбцов по имени в выборке
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int surnameColIndex = c.getColumnIndex("surname");
+                    int phoneColIndex = c.getColumnIndex("phone");
+
+                    do {
+                        // получаем значения по номерам столбцов и пишем все в лог
+                        Log.d(LOG_TAG,
+                                "ID = " + c.getInt(idColIndex) +
+                                        ", name = " + c.getString(nameColIndex) +
+                                        ", surname = " + c.getString(surnameColIndex) +
+                                        ", phone = " + c.getString(phoneColIndex));
+                        // переход на следующую строку
+                        // а если следующей нет (текущая - последняя), то false - выходим из цикла
+                    } while (c.moveToNext());
+                } else
+                    Log.d(LOG_TAG, "0 rows");
+                c.close();
+                break;
+
+            case R.id.btnClear:
+                Log.d(LOG_TAG, "--- Clear mytable: ---");
+                // удаляем все записи
+                int clearCount = db.delete("mytable", null, null);
+                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+                break;
+            // закрываем подключение к БД
+
+        }
+        dbHelper.close();
+    }
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            // конструктор суперкласса
+            super(context, "personDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d("my_log", "--- onCreate database ---");
+            // создаем таблицу с полями
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "name text,"
+                    + "surname text,"
+                    + "phone text" + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+}
